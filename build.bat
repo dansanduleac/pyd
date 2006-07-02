aa587e34d60bda440ecdd342cf56c91f9fdbce22
KirkMcDonald <KirkMcDonald@1df65b71-e716-0410-9316-ac55df2b1602>
2006-07-02 01:04:24 +0000

    Wipe in prep for reorganizational update...
    git-svn-id: http://svn.dsource.org/projects/pyd/trunk@23 1df65b71-e716-0410-9316-ac55df2b1602

--- build.bat
+++
@@ -1,9 +0,0 @@
-SET D_ROOT=C:\dmd
-SET PY_ROOT=C:\Python24
-
-SET PATH=%D_ROOT%\dm\bin;%D_ROOT%\dmd\bin;%PY_ROOT%;%PATH%
-
-del .\build\lib.win32-2.4\testdll.pyd
-python.exe setup.py build
-pause
-
--- build_ddoc.bat
+++
@@ -1,3 +0,0 @@
-C:\dmd\dmd\bin\dmd.exe -o- -Ddpyd\doc pyd\class_wrap.d pyd\ctor_wrap.d pyd\def.d pyd\dg_convert.d pyd\exception.d pyd\ftype.d pyd\lazy_load.d pyd\make_object.d pyd\object.d pyd\pyd.d pyd\doc\candydoc\candy.ddoc pyd\doc\candydoc\modules.ddoc
-
-pause
