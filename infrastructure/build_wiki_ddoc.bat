415cf00790049611ba2e7f2bbd9d054d964b5d8c
KirkMcDonald <KirkMcDonald@1df65b71-e716-0410-9316-ac55df2b1602>
2006-08-10 22:22:04 +0000

    html_doc: Documented class wrapping, DPyObject
    git-svn-id: http://svn.dsource.org/projects/pyd/trunk@34 1df65b71-e716-0410-9316-ac55df2b1602

--- infrastructure/build_wiki_ddoc.bat
+++ infrastructure/build_wiki_ddoc.bat
@@ -1,3 +1,3 @@
-C:\dmd\dmd\bin\dmd.exe -o- -Ddpyd\wiki_doc pyd\class_wrap.d pyd\ctor_wrap.d pyd\def.d pyd\dg_convert.d pyd\exception.d pyd\ftype.d pyd\make_object.d pyd\dpyobject.d pyd\pyd.d -Ipython\headers
+C:\dmd\dmd\bin\dmd.exe -o- -Ddpyd\wiki_doc pyd\class_wrap.d pyd\ctor_wrap.d pyd\def.d pyd\dg_convert.d pyd\dpyobject.d pyd\exception.d pyd\ftype.d pyd\make_object.d pyd\op_wrap.d pyd\pyd.d pyd\tuples.d -Ipython\headers
 
 pause
